981af0fe3f4d6ea58f98a4802bf5c66b488e68a7
bokuweb <info@typebeats.com>
2015-04-13 09:08:47 +0900

    add init func

--- main.c
+++ main.c
@@ -19,20 +19,35 @@
 
 /* Private Functions ----------------------------------------------- */
 
-/* Public Functions ------------------------------------------------ */
-
-int16_t main(void)
+/******************************************************************//**
+ * @brief       initialize
+ * @param       none
+ * @return      none
+ *********************************************************************/
+static void init(void)
 {
     LED1_PIN = 0; // Make Pin as O/P
     LED1 = 0;     // Turn LED ON
 
     serial_init();
+}
+
+/* Public Functions ------------------------------------------------ */
+/******************************************************************//**
+ * @brief       initialize serial function
+ * @param       none
+ * @return      none
+ *********************************************************************/    
+int16_t main(void)
+{
+    init();
+
     kz_puts("Hello World!\n");
     kz_putxval(0x10, 0);
     kz_puts("\n");
     kz_putxval(0xffff, 0);
     kz_puts("\n");
-    
+
     while (true) {
         ;
     }
